686b4ec14bf9d7116543a8f5e8c782776e27b6fa
S-123-satya <satyaprakash5056742@gmail.com>
2023-11-16 13:17:51 +0530

    meal summary completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Layout/Header';
+import MealsSummary from './components/cart/MealsSummary';
 
 function App() {
   return (
     <>
     <Header/>
+    <MealsSummary/>
     </>
   );
 }
